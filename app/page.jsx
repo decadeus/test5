457bbc1a56b6aaa03cd29d25f666408c910b8e32
decadeus <debeaumont@decadeus.com>
2024-06-03 16:13:14 +0200

    again

--- app/page.jsx
+++ app/page.jsx
@@ -6,6 +6,8 @@ import Link from "next/link";
 import { Button } from "@nextui-org/react";
 
 export default function TodoList() {
+  const supabase = createClient();
+  const [todos, setTodos] = useState([]);
 
   useEffect(() => {
     async function fetchTodos() {
