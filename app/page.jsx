b49a0b13a99dcfc94cdbf8ccf5879dac30728be5
decadeus <debeaumont@decadeus.com>
2024-06-03 15:15:40 +0200

    statuster

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ export function page() {
 
   return (<>
   <Link rel="stylesheet" href="/test">link</Link>
-    <div className='bg-red-300'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo eaque iste modi officiis in similique sint impedit aliquid id harum, ducimus, earum esse voluptatem labore saepe culpa delenit,njni aliquaddd quia!</div>
+    <div className='bg-red-300'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo eaque iste modi officiis in similique sint impedit aliquid id harum, ducimus, earum esse voluptatem labore saepe cultjtypa delenit,njni aliquaddd quia!</div>
     </>
   )
 }
