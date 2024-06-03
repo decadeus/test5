20ea04d34c3d222a0d79ca3171505f0faf44762f
decadeus <debeaumont@decadeus.com>
2024-06-03 15:46:48 +0200

    page link

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Link from 'next/link'
 
-function page() {
-  return (
-    <div>page</div>
+
+export default function page() {
+
+  return (<>
+  <Link rel="stylesheet" href="/test">link</Link>
+    <div className='bg-red-300'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo eaque iste modi officiis in similique sint impedit aliquid id harum, ducimus, earum esse voluptatem labore saepe culpa deleniti aliquam quia!</div>
+    </>
   )
 }
-
-export default page
