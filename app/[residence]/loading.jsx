1d67c714a8950721b77ddab17e4b049c32d3be61
decadeus <debeaumont@decadeus.com>
2024-06-03 16:45:49 +0200

    again

--- app/[residence]/loading.jsx
+++ app/[residence]/loading.jsx
@@ -1,31 +1,10 @@
-import React from "react";
-import { createClient } from "./../../utils/supabase/client";
-import { log } from "console";
-import Appart from "./appart";
-
-export default async function page({ params }) {
-  const supabase = createClient();
-
-  const value = params.residence.replace(/_/g, " ");
-  log(value);
-  const { data, error } = await supabase
-    .from("residence")
-    .select("*")
-    .eq("Description", value)
-    .single();
-
-  if (error) {
-    // Handle error
-  }
-
-  return (
-    <div>
-      <div className="flex flex-col justify-center items-center">
-        <h1 className="text-4xl font-bold">{data && data.residence}</h1>
-        {data && data.city}
+export default function Loading() {
+    return (
+      <div>
+        <div className="flex flex-col justify-center items-center">
+        <h1 className="text-4xl font-bold">...Loading</h1>
+        Loading
       </div>
-
-      <Appart value={data && data.id} />
-    </div>
-  );
-}
+      </div>
+    );
+  }
--- app/[residence]/page.jsx
+++ app/[residence]/page.jsx
@@ -1,15 +1,31 @@
 import React from "react";
-import Front from "./front/page";
+import { createClient } from "./../../utils/supabase/client";
+import { log } from "console";
+import Appart from "./appart";
 
-function page({ params }) {
+export default async function page({ params }) {
+  const supabase = createClient();
+
+  const value = params.residence.replace(/_/g, " ");
+  log(value);
+  const { data, error } = await supabase
+    .from("residence")
+    .select("*")
+    .eq("Description", value)
+    .single();
+
+  if (error) {
+    // Handle error
+  }
+
   return (
     <div>
-     <div className="bg-blue-300">
-        <Front />
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-4xl font-bold">{data && data.residence}</h1>
+        {data && data.city}
       </div>
-      pagr {params.residence}
+
+      <Appart value={data && data.id} />
     </div>
   );
 }
-
-export default page;
