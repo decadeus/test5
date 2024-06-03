40e4c979f88462fbccce63c1975f144d3c6f70b4
decadeus <debeaumont@decadeus.com>
2024-06-03 15:20:42 +0200

    linkremove

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import Link from 'next/link'
+
 
 
 export function page() {
 
   return (<>
-  <Link rel="stylesheet" href="/test">link</Link>
-    <div className='bg-red-300'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo eaque iste modi officiis in similique sint impedit aliquid id harum, ducimus, earum esse voluptatem labore saepe cultjtypa delenit,njni aliquaddd quia!</div>
-    </>
+  <p>jlkjlkjl</p></>
   )
 }
