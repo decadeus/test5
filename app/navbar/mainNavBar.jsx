b38e9e49e8007d4125e188140a461a0ca9811645
decadeus <debeaumont@decadeus.com>
2024-06-24 11:23:53 +0200

    test

--- app/navbar/mainNavBar.jsx
+++ app/navbar/mainNavBar.jsx
@@ -12,11 +12,11 @@ export default async function MainNavBar({ user }) {
   // Fetch user profile data if user is defined
   const profile = user ? await fetchUserProfile(supabase, user.id) : null;
 
-  // const { data, error } = await supabase
-  // .from("profiles")
-  // .select("avatar_url")
-  // .eq("id", user.id)
-  // .single()
+  const { data, error } = await supabase
+  .from("profiles")
+  .select("avatar_url")
+  .eq("id", user.id)
+  .single()
 
   return (
     <div className="w-full px-10 py-4 flex justify-between items-center border-b mb-8 text-black">
@@ -49,7 +49,7 @@ export default async function MainNavBar({ user }) {
       <div>
         {user ? (
           <div className="flex gap-2 items-center">
-            {/* <HelpAdmin /> */}
+          <HelpAdmin />
             <div className="flex flex-col text-center">
               <p className="font-bold">{profile?.username}</p>
               <p>{user.email}</p>
@@ -131,12 +131,12 @@ const siteConfig = {
   ],
 };
 
-// function HelpAdmin() {
-//   return (
-//     <div className="pr-8">
-//       <Tooltip content="Aide" className="bg-black text-white">
-//       <h1 className=" text-center flex justify-center items-center font-bold rounded-full border text-white bg-green-500 w-8 h-8">?</h1>
-//       </Tooltip>
-//     </div>
-//   )
-// }
+function HelpAdmin() {
+  return (
+    <div className="pr-8">
+      <Tooltip content="Aide" className="bg-black text-white">
+      <h1 className=" text-center flex justify-center items-center font-bold rounded-full border text-white bg-green-500 w-8 h-8">?</h1>
+      </Tooltip>
+    </div>
+  )
+}
