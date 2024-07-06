b92a97056cdc06284779d2658c0d4547f5819e42
decadeus <debeaumont@decadeus.com>
2024-07-06 17:46:53 +0200

    test

--- components/map.jsx
+++ components/map.jsx
@@ -27,11 +27,12 @@ const MapComponent = () => {
   const [icon, setIcon] = useState(null);
 
   useEffect(() => {
+    // Only run the Leaflet configuration on the client side
     if (typeof window !== "undefined") {
       const newIcon = new L.Icon({
         iconUrl: customMarkerIcon.src || customMarkerIcon,
         iconRetinaUrl: customMarkerIcon.src || customMarkerIcon,
-        iconSize: [25, 41],
+        iconSize: [41, 41],
         iconAnchor: [12, 41],
         popupAnchor: [1, -34],
         shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
