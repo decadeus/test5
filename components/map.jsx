cf056e9371086bd77584f379d6b21e05c0aad062
decadeus <debeaumont@decadeus.com>
2024-07-06 15:23:16 +0200

    test leaf

--- components/map.jsx
+++ components/map.jsx
@@ -1,41 +1,49 @@
 "use client";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import L from "leaflet";
-import MarkerIcon from "../node_modules/leaflet/dist/images/marker-icon.png";
-import MarkerShadow from "../node_modules/leaflet/dist/images/marker-shadow.png";
-import "leaflet/dist/leaflet.css";
+import React from "react";
+import dynamic from "next/dynamic";
 
-const Map = () => {
+// Dynamic import of react-leaflet components
+const MapContainer = dynamic(
+  () => import("react-leaflet").then((module) => module.MapContainer),
+  {
+    ssr: false, // Disable server-side rendering for this component
+  }
+);
+const TileLayer = dynamic(
+  () => import("react-leaflet").then((module) => module.TileLayer),
+  {
+    ssr: false,
+  }
+);
+const Marker = dynamic(
+  () => import("react-leaflet").then((module) => module.Marker),
+  {
+    ssr: false,
+  }
+);
+const Popup = dynamic(
+  () => import("react-leaflet").then((module) => module.Popup),
+  {
+    ssr: false,
+  }
+);
+
+const MapComponent = () => {
   return (
-    <div>
-      <MapContainer
-        style={{ height: "50vh", width: "50vw" }}
-        center={[51.505, -0.09]}
-        zoom={13}
-        scrollWheelZoom={false}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker
-          icon={
-            new L.Icon({
-              iconUrl: MarkerIcon.src,
-              iconRetinaUrl: MarkerIcon.src,
-              iconSize: [25, 41],
-              iconAnchor: [12.5, 41],
-              popupAnchor: [0, -41],
-              shadowUrl: MarkerShadow.src,
-              shadowSize: [41, 41],
-            })
-          }
-          position={[51.505, -0.09]}
-        ><Popup>
-            <p>Magnifique ici</p></Popup></Marker>
-      </MapContainer>
-    </div>
+    <MapContainer
+      center={[37.7577, -122.4376]}
+      zoom={8}
+      style={{ width: "50vw", height: "50vh" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={[37.7577, -122.4376]}>
+        <Popup>User Post Location</Popup>
+      </Marker>
+    </MapContainer>
   );
 };
 
-export default Map;
+export default MapComponent;
