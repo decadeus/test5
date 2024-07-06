2d37c9ea195e2717206f4728f6b62639195df39e
decadeus <debeaumont@decadeus.com>
2024-07-06 16:53:30 +0200

    test

--- components/map.jsx
+++ components/map.jsx
@@ -1,11 +1,7 @@
 "use client";
 import React from "react";
 import dynamic from "next/dynamic";
-import L from "leaflet";
-import MarkerIcon from "../node_modules/leaflet/dist/images/marker-icon.png"
-import MarkerShadow from "../node_modules/leaflet/dist/images/marker-shadow.png"
 import 'leaflet/dist/leaflet.css'
-
 
 // Dynamic import of react-leaflet components
 const MapContainer = dynamic(
@@ -36,28 +32,15 @@ const Popup = dynamic(
 const MapComponent = () => {
   return (
     <MapContainer
-    center={[37.7577, -122.4376]}
-    zoom={8}
-    style={{ width: "50vw", height: "50vh" }}
+      center={[37.7577, -122.4376]}
+      zoom={8}
+      style={{ width: "50vw", height: "50vh" }}
     >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-       <Marker
-        icon={
-          new L.Icon({
-            iconUrl: MarkerIcon.src,
-            iconRetinaUrl: MarkerIcon.src,
-            iconSize: [25, 41],
-            iconAnchor: [12, 41],
-            popupAnchor: [1, -34],
-            shadowUrl: MarkerShadow.src,
-            shadowSize: [41, 41],
-          })
-        }
-        position={[37.7577, -122.4376]}
-      >
+      <Marker position={[37.7577, -122.4376]}>
         <Popup>User Post Location</Popup>
       </Marker>
     </MapContainer>
