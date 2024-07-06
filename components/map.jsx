89544b22758b24406015cf454d00b55c4ea751fb
decadeus <debeaumont@decadeus.com>
2024-07-06 16:33:51 +0200

    map test

--- components/map.jsx
+++ components/map.jsx
@@ -1,6 +1,10 @@
 "use client";
 import React from "react";
 import dynamic from "next/dynamic";
+import L from "leaflet";
+import MarkerIcon from "../node_modules/leaflet/dist/images/marker-icon.png";
+import MarkerShadow from "../node_modules/leaflet/dist/images/marker-shadow.png";
+import 'leaflet/dist/leaflet.css'
 
 // Dynamic import of react-leaflet components
 const MapContainer = dynamic(
@@ -9,6 +13,8 @@ const MapContainer = dynamic(
     ssr: false, // Disable server-side rendering for this component
   }
 );
+
+
 const TileLayer = dynamic(
   () => import("react-leaflet").then((module) => module.TileLayer),
   {
@@ -39,7 +45,20 @@ const MapComponent = () => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={[37.7577, -122.4376]}>
+      <Marker
+        icon={
+          new L.Icon({
+            iconUrl: MarkerIcon.src,
+            iconRetinaUrl: MarkerIcon.src,
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowUrl: MarkerShadow.src,
+            shadowSize: [41, 41],
+          })
+        }
+        position={[37.7577, -122.4376]}
+      >
         <Popup>User Post Location</Popup>
       </Marker>
     </MapContainer>
