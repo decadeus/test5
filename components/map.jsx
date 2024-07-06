a620fef52db1774006e39eb508f1fef6dccafe8f
decadeus <debeaumont@decadeus.com>
2024-07-06 17:18:36 +0200

    test

--- components/map.jsx
+++ components/map.jsx
@@ -1,39 +1,47 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
-import 'leaflet/dist/leaflet.css'
+import 'leaflet/dist/leaflet.css';
 import L from "leaflet";
-import MarkerIcon from "../node_modules/leaflet/dist/images/marker-icon.png"
-import MarkerShadow from "../node_modules/leaflet/dist/images/marker-shadow.png"
-
+import customMarkerIcon from '@/components/b.png'; // Adjust the path as necessary
 
 // Dynamic import of react-leaflet components
 const MapContainer = dynamic(
   () => import("react-leaflet").then((module) => module.MapContainer),
-  {
-    ssr: false, // Disable server-side rendering for this component
-  }
+  { ssr: false }
 );
 const TileLayer = dynamic(
   () => import("react-leaflet").then((module) => module.TileLayer),
-  {
-    ssr: false,
-  }
+  { ssr: false }
 );
 const Marker = dynamic(
   () => import("react-leaflet").then((module) => module.Marker),
-  {
-    ssr: false,
-  }
+  { ssr: false }
 );
 const Popup = dynamic(
   () => import("react-leaflet").then((module) => module.Popup),
-  {
-    ssr: false,
-  }
+  { ssr: false }
 );
 
 const MapComponent = () => {
+  const [icon, setIcon] = useState(null);
+
+  useEffect(() => {
+    // Only run the Leaflet configuration on the client side
+    if (typeof window !== "undefined") {
+      const newIcon = new L.Icon({
+        iconUrl: customMarkerIcon.src,
+        iconRetinaUrl: customMarkerIcon.src,
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+        shadowSize: [41, 41],
+      });
+      setIcon(newIcon);
+    }
+  }, []);
+
   return (
     <MapContainer
       center={[37.7577, -122.4376]}
@@ -44,24 +52,11 @@ const MapComponent = () => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-       <Marker
-        icon={
-          new L.Icon({
-            iconUrl: MarkerIcon.src,
-            iconRetinaUrl: MarkerIcon.src,
-            iconSize: [25, 41],
-            iconAnchor: [12, 41],
-            popupAnchor: [1, -34],
-            shadowUrl: MarkerShadow.src,
-            shadowSize: [41, 41],
-          })
-        }
-        position={[37.7577, -122.4376]}
-      >
-        <Popup>User Post Location</Popup>
-      </Marker>
-      
-      
+      {icon && (
+        <Marker position={[37.7577, -122.4376]} icon={icon}>
+          <Popup>User Post Location</Popup>
+        </Marker>
+      )}
     </MapContainer>
   );
 };
