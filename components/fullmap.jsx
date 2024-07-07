c5fdb9420864cd7078bdc36b0eeedf158d30d364
decadeus <debeaumont@decadeus.com>
2024-07-07 16:00:11 +0200

    map popup

--- components/fullmap.jsx
+++ components/fullmap.jsx
@@ -1,8 +1,10 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
-import 'leaflet/dist/leaflet.css';
-import { createClient } from "@/utils/supabase/client"; // Ensure supabase-js is installed and imported
+import "leaflet/dist/leaflet.css";
+import { createClient } from "@/utils/supabase/client";
+import Avatar from "@/app/getimage/Ugetone";
+import Link from "next/link"; // Ensure supabase-js is installed and imported
 
 // Dynamic import of react-leaflet components
 const MapContainer = dynamic(
@@ -46,18 +48,40 @@ const MapComponent = ({ lata, lnga, classN }) => {
   }, []);
 
   return (
-    <MapContainer
-      center={[52.07957, 20.97848]}
-      zoom={12}
-      className={classN}
-    >
+    <MapContainer center={[52.07957, 20.97848]} zoom={12} className={classN}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
       {todos.map((todo) => (
-        <Marker key={todo} position={[todo.lat, todo.lng]} style={{ color: "lightblue" }}>
-          <Popup>{todo.city}</Popup> {/* Assuming todo has a 'name' property */}
+        <Marker
+          key={todo}
+          position={[todo.lat, todo.lng]}
+          style={{ color: "lightblue" }}
+        >
+          <Popup maxWidth={500}>
+            <div className="flex-row w-full">
+              <div className="w-[150px] aspect-square rounded-md">
+                <Avatar
+                  url={todo.mainpic_url}
+                  width={350}
+                  height={350}
+                  className="w-[350px] h-[350px] rounded-md"
+                  style={{
+                    objectFit: "cover",
+                  
+                    width: "350px",
+                    height: "350px",
+                  }}
+                />
+              </div>
+              
+
+              <div className="pt-2">{todo.city}</div>
+              <Link href={`/${todo.id}`}><p className="font-bold">See</p></Link>
+            </div>
+          </Popup>{" "}
+          {/* Assuming todo has a 'name' property */}
         </Marker>
       ))}
     </MapContainer>
