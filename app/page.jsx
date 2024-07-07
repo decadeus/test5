3838c338636169b6c502ee47d39c0c399a0846ca
decadeus <debeaumont@decadeus.com>
2024-07-07 23:24:46 +0200

    mainpage

--- app/page.jsx
+++ app/page.jsx
@@ -3,86 +3,111 @@ import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { createClient } from "./../utils/supabase/client";
 import Avatar from "./getimage/Ugetone";
-import GoogleMaps from "./[residence]/googlemap";
-import Map from "@/components/fullmap"
-
+import Map from "@/components/fullmap";
+import Image from "next/image";
+import image from "@/components/image/appart3.jpg";
 
 export default function TodoList() {
   const supabase = createClient();
   const [todos, setTodos] = useState([]);
 
   useEffect(() => {
-    async function fetchData() {
-      const { data: residences, error: residenceError } = await supabase.from("residence").select("*");
-      if (residenceError) {
-        console.log("error", residenceError);
-        return;
+    // Function to fetch data from Supabase
+    const fetchData = async () => {
+      try {
+        const [residenceResponse, apartmentResponse] = await Promise.all([
+          supabase.from("residence").select("*"),
+          supabase.from("appartement").select("type, ida"),
+        ]);
+
+        const { data: residences, error: residenceError } = residenceResponse;
+        const { data: apartments, error: apartmentError } = apartmentResponse;
+
+        if (residenceError) {
+          console.error("Error fetching residences:", residenceError);
+          return;
+        }
+
+        if (apartmentError) {
+          console.error("Error fetching apartments:", apartmentError);
+          return;
+        }
+
+        // Combine residences with their associated apartments
+        const residencesWithCounts = residences.map((residence) => {
+          const residenceApartments = apartments.filter(
+            (apt) => apt.ida === residence.id
+          );
+          const countVendre = residenceApartments.filter(
+            (apt) => apt.type === "To sell"
+          ).length;
+          const countLouer = residenceApartments.filter(
+            (apt) => apt.type === "To rent"
+          ).length;
+
+          return {
+            ...residence,
+            countVendre,
+            countLouer,
+          };
+        });
+
+        setTodos(residencesWithCounts);
+      } catch (error) {
+        console.error("Error fetching data:", error);
       }
-
-      // Fetch apartments and associate them with residences
-      const { data: apartments, error: apartmentError } = await supabase.from("appartement").select("type, ida");
-      if (apartmentError) {
-        console.log("error", apartmentError);
-        return;
-      }
-
-      // Combine residences with their associated apartments
-      const residencesWithCounts = residences.map(residence => {
-        const residenceApartments = apartments.filter(apt => apt.ida === residence.id);
-        const countVendre = residenceApartments.filter(apt => apt.type === 'To sell').length;
-        const countLouer = residenceApartments.filter(apt => apt.type === 'To rent').length;
-        
-        return {
-          ...residence,
-          countVendre,
-          countLouer
-        };
-      });
-
-      setTodos(residencesWithCounts);
-    }
+    };
 
     fetchData();
-  }, []);
+  }, [supabase]);
 
   return (
-    <div className="w-full flex-col">
-      <Map classN="w-full h-[300px]" />
-       
-      <div className=" w-full px-4 md:px-16">
-        <ul className="flex flex-col md:flex-row md:flex-wrap md:justify-start gap-8 pt-16 ">
-          {todos.map((todo) => (
-            
-            <li key={todo.id} className=" justify-center">
-              <Link href={`/${todo.id}`}>
-              <div className="flex gap-4 md:flex-col">
-                <div className="aspect-square w-1/2 md:w-[250px]">
-                  <Avatar
-                    url={todo.mainpic_url}
-                    width={270}
-                    height={196}
-                    classn="rounded-2xl"
-                  />
-                </div>
-                <div className="flex flex-col gap-0 pt-2 w-1/2">
-                  <p className="font-bold">{todo.mainTitle}</p>
-                  <p>{todo.city}</p>
-                  <p className="text-sm">To sell: {todo.countVendre}</p>
-                  <p className="text-sm">To rent: {todo.countLouer}</p>
-                </div>
-                </div>
-              </Link>
-            </li>
-          ))}
-        </ul>
+    <div className="w-full px-8 md:px-32">
+      <div className="flex justify-center w-full h-[300px] z-0 rounded-2xl relative">
+        <div
+          className="relative overflow-hidden rounded-lg bg-cover bg-no-repeat p-12  w-full"
+          style={{
+            backgroundImage: `url(${image.src})`,
+            height: "400px",
+          }}
+        >
+          <p className="text-white font-bold text-2xl">Chercher un logement</p>
+        </div>
       </div>
-     
-      <div className="w-full lg:w:1/3">
-        <GoogleMaps
-          lata="-9.069839810859907"
-          lnga="39.60128890889341"
-          height="h-full"
-        />
+      <div className="w-full flex z-10 -mt-16 justify-center px-16">
+        <div className="w-2/3 px-4 md:px-16 rounded-2xl z-10">
+          <ul className="flex flex-col gap-8 pt-8 bg-gray-200 p-4 rounded-2xl">
+            <p className="text-center">Top</p>
+            {todos.map((todo) => (
+              <li
+                key={todo.id}
+                className="border bg-white p-4 rounded-2xl"
+              >
+                <Link href={`/${todo.id}`}>
+                  <div className="flex gap-4">
+                    <div className="w-1/2 h-36">
+                      <Avatar
+                        url={todo.mainpic_url}
+                        width={270}
+                        height={196}
+                        classn="rounded-2xl"
+                      />
+                    </div>
+                    <div className="flex flex-col gap-2 w-1/2">
+                      <p className="font-bold">{todo.mainTitle}</p>
+                      <p>{todo.city}</p>
+                      <p className="text-sm">To sell: {todo.countVendre}</p>
+                      <p className="text-sm">To rent: {todo.countLouer}</p>
+                    </div>
+                  </div>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="w-1/3">
+          <Map classN="w-full h-full rounded-2xl" />
+        </div>
       </div>
     </div>
   );
--- components/map.jsx
+++ components/map.jsx
@@ -1,8 +1,7 @@
 "use client";
 import React from "react";
 import dynamic from "next/dynamic";
-import 'leaflet/dist/leaflet.css'
-
+import "leaflet/dist/leaflet.css";
 
 // Dynamic import of react-leaflet components
 const MapContainer = dynamic(
@@ -30,22 +29,16 @@ const Popup = dynamic(
   }
 );
 
-const MapComponent = ({lata, lnga, classN}) => {
+const MapComponent = ({ lata, lnga, classN }) => {
   return (
-    <MapContainer
-      center={[lata, lnga]}
-      zoom={12}
-     className={classN}
-    >
+    <MapContainer center={[lata, lnga]} zoom={12} className={classN}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={[lata, lnga]} style={{color: "lightblue"}}>
+      <Marker position={[lata, lnga]} style={{ color: "lightblue" }}>
         <Popup>User Post Location</Popup>
       </Marker>
-      
-      
     </MapContainer>
   );
 };
