f249ab01063f52cd28a94042e1c4ae27a7af439a
decadeus <debeaumont@decadeus.com>
2024-07-10 13:03:02 +0200

    projet

--- app/(pages)/projets/page.jsx
+++ app/(pages)/projets/page.jsx
@@ -2,9 +2,7 @@
 import {
   Modal,
   ModalContent,
-  ModalHeader,
   ModalBody,
-  ModalFooter,
   Button,
   useDisclosure,
 } from "@nextui-org/react";
@@ -12,6 +10,7 @@ import React from "react";
 import { FaPlus } from "react-icons/fa6";
 import Image from "next/image";
 import Im from "@/components/image/appart3.jpg";
+import archi from "@/components/image/plan_archi.jpg";
 
 export default function Page() {
   // Define a constant map with song data
@@ -23,7 +22,25 @@ export default function Page() {
       level: "RDC",
       Price: "15 000 Pln",
       Detail: "Details",
-      songAData: [{ bien: "RDC", Pabien: "15 000 Pln" }],
+      songAData: {
+        ref: "ref: 233N4BB",
+        DateDispo: "Livraison prevu pour le 12/09/2025",
+        City: "Warsaw",
+        Residence: "Residence name",
+        PriceP: "230 000 Pln",
+        rooms: "3 pieces",
+        surface: "54 m2",
+        Ann1: "Ann1",
+        Ann2: "Ann2",
+        Ann3: "Ann3",
+        Ann4: "Ann4",
+        Ann5: "Ann5",
+        tAnn1: "dAnn1",
+        tAnn2: "dAnn2",
+        tAnn3: "dAnn3",
+        tAnn4: "dAnn4",
+        tAnn5: "dAnn5",
+      },
     },
     {
       rooms: 2,
@@ -32,7 +49,25 @@ export default function Page() {
       level: 2,
       Price: "1000",
       Detail: "Details",
-      songAData: [{ bien: "RDC", Pabien: "15 000 Pln" }],
+      songAData: {
+        ref: "ref",
+        DateDispo: "Livraison prevu pour le 12/09/2025",
+        City: "Warsaw",
+        Residence: "Residence name",
+        PriceP: "230 000 Pln",
+        rooms: "3 pieces",
+        surface: "54 m2",
+        Ann1: "Ann1",
+        Ann2: "Ann2",
+        Ann3: "Ann3",
+        Ann4: "Ann4",
+        Ann5: "Ann5",
+        tAnn1: "dAnn1",
+        tAnn2: "dAnn2",
+        tAnn3: "dAnn3",
+        tAnn4: "dAnn4",
+        tAnn5: "dAnn5",
+      },
     },
     {
       rooms: 4,
@@ -41,7 +76,25 @@ export default function Page() {
       level: 20,
       Price: "1000",
       Detail: "Details",
-      songAData: [{ bien: "RDC", Pabien: "15 000 Pln" }],
+      songAData: {
+        ref: "ref",
+        DateDispo: "Livraison prevu pour le 12/09/2025",
+        City: "Warsaw",
+        Residence: "Residence name",
+        PriceP: "230 000 Pln",
+        rooms: "3 pieces",
+        surface: "54 m2",
+        Ann1: "Ann1",
+        Ann2: "Ann2",
+        Ann3: "Ann3",
+        Ann4: "Ann4",
+        Ann5: "Ann5",
+        tAnn1: "dAnn1",
+        tAnn2: "dAnn2",
+        tAnn3: "dAnn3",
+        tAnn4: "dAnn4",
+        tAnn5: "dAnn5",
+      },
     },
     {
       rooms: 3,
@@ -50,7 +103,25 @@ export default function Page() {
       level: 4,
       Price: "1000",
       Detail: "Details",
-      songAData: [{ bien: "RDC", Pabien: "15 000 Pln" }],
+      songAData: {
+        refA: "ref",
+        DateDispo: "DateDispo",
+        City: "City",
+        Residence: "Residence",
+        PriceP: "Price",
+        Rooms: "Rooms",
+        surface: "surface",
+        Ann1: "Ann1",
+        Ann2: "Ann2",
+        Ann3: "Ann3",
+        Ann4: "Ann4",
+        Ann5: "Ann5",
+        tAnn1: "dAnn1",
+        tAnn2: "dAnn2",
+        tAnn3: "dAnn3",
+        tAnn4: "dAnn4",
+        tAnn5: "dAnn5",
+      },
     },
   ];
 
@@ -73,43 +144,72 @@ function Appartement({ songs }) {
       >
         <div className="absolute inset-0 "></div>
         <div className="bg-black w-fit p-16 flex justify-start">
-        <p className="relative text-white font-bold text-3xl z-10">
-          Find your dream apartment <br /> and residence
-        </p>
+          <p className="relative text-white font-bold text-3xl z-10">
+            Find your dream apartment <br /> and residence
+          </p>
         </div>
       </div>
       <div className="flex flex-col gap-64">
-      <div className="flex px-32 gap-8 ">
-        <div className="w-1/2 flex justify-center items-center">
-          <div>
-            <h2>Lorem ipsum dolor sit </h2>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo, non dolorem illo aperiam debitis, reiciendis facilis neque facere repellendus aut sequi laudantium odio natus ipsam ratione, sed ex quas accusantium.</p>
+        <div className="flex px-32 gap-8 ">
+          <div className="w-1/2 flex justify-center items-center">
+            <div>
+              <h2>Lorem ipsum dolor sit </h2>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo,
+                non dolorem illo aperiam debitis, reiciendis facilis neque
+                facere repellendus aut sequi laudantium odio natus ipsam
+                ratione, sed ex quas accusantium.
+              </p>
+            </div>
+          </div>
+          <div className="w-1/2 h-[300px] ">
+            <Image src={Im} alt="appart" />
           </div>
         </div>
-        <div className="w-1/2 h-[300px] ">
-          <Image src={Im} alt="appart" />
-        </div>
-      </div>
-      <div className="flex flex-row-reverse px-32 gap-8 ">
-        <div className="w-1/2 flex justify-center items-center ">
-          <div>
-          <h2>Lorem ipsum dolor sit </h2>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo, non dolorem illo aperiam debitis, reiciendis facilis neque facere repellendus aut sequi laudantium odio natus ipsam ratione, sed ex quas accusantium.</p>
+        <div className="flex flex-row-reverse px-32 gap-8 ">
+          <div className="w-1/2 flex justify-center items-center ">
+            <div>
+              <h2>Lorem ipsum dolor sit </h2>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo,
+                non dolorem illo aperiam debitis, reiciendis facilis neque
+                facere repellendus aut sequi laudantium odio natus ipsam
+                ratione, sed ex quas accusantium.
+              </p>
+            </div>
+          </div>
+          <div className="w-1/2 h-[400px] ">
+            <Image src={Im} alt="appart" />
           </div>
         </div>
-        <div className="w-1/2 h-[400px] ">
-          <Image src={Im} alt="appart"/>
-        </div>
       </div>
-      </div>
-      <div>
+      <div className="pt-16">
         <Tab songs={songs} />
       </div>
     </div>
   );
 }
 
-function Pop({ Price }) {
+function Pop({
+  Price,
+  DateDispo,
+  City,
+  Residence,
+  PriceP,
+  Rooms,
+  surface,
+  Ann1,
+  Ann2,
+  Ann3,
+  Ann4,
+  Ann5,
+  tAnn1,
+  tAnn2,
+  tAnn3,
+  tAnn4,
+  tAnn5,
+  refA,
+}) {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
   return (
     <>
@@ -120,21 +220,56 @@ function Pop({ Price }) {
       >
         <FaPlus />
       </Button>
-      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+      <Modal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        size="4xl"
+        placement="top"
+        className="px-0 py-0"
+      >
         <ModalContent>
           {(onClose) => (
             <>
-              <ModalBody>
-                <p className="text-black">{Price}</p>
+              <ModalBody className="w-full py-8 px-8">
+                <div className="bg-gradient-to-br from-sky-900 to-blue-400 text-white w-full p-4 rounded-xl ">
+                  <div className="flex justify-between items-center">
+                    
+                    <div className="flex items-center gap-16">
+                      <p className="text-2xl font-bold">{Price}</p>
+                      <p>{DateDispo}</p>
+                    </div>
+                    <div>
+                      <p>{refA}</p>
+                    </div>
+                  </div>
+                  <p className="text-2x font-semibold">{City}</p>
+                  <p className="text-xl font-semibold">{Residence}</p>
+
+                  <div className="flex gap-4">
+                    <p>{Rooms}</p> <p>{surface}</p>
+                  </div>
+                </div>
+                <div className="w-full ">
+                  <Image src={archi} alt="appart" />
+
+                  <div className="flex">
+                    <div className="font-bold">
+                      <p>{Ann1}</p>
+                      <p>{Ann2}</p>
+                      <p>{Ann3}</p>
+                      <p>{Ann4}</p>
+                      <p>{Ann5}</p>
+                    </div>
+                    <div className="">
+                      <p>{tAnn1}</p>
+                      <p>{tAnn2}</p>
+                      <p>{tAnn3}</p>
+                      <p>{tAnn4}</p>
+                      <p>{tAnn5}</p>
+                    </div>
+                  </div>
+                </div>
               </ModalBody>
-              <ModalFooter>
-                <Button color="danger" variant="light" onPress={onClose}>
-                  Close
-                </Button>
-                <Button color="primary" onPress={onClose}>
-                  Action
-                </Button>
-              </ModalFooter>
             </>
           )}
         </ModalContent>
@@ -144,6 +279,8 @@ function Pop({ Price }) {
 }
 
 function Tab({ songs }) {
+  const colorB = "border-violet-900";
+  const ps = "text-black font-bold";
   return (
     <div className="overflow-x-auto bg-white md:px-64">
       <table
@@ -192,32 +329,48 @@ function Tab({ songs }) {
         </thead>
         <tbody>
           {songs.map((song, index) => (
-            <tr key={index} className="group hover:bg-slate-200">
+            <tr key={index} className="group hover:bg-violet-200">
               <td
                 scope="row"
-                className="px-6 py-3 border-t border-b border-l rounded-tl-lg rounded-bl-lg border-red-600 "
+                className={`px-6 py-3 border-t-4 border-b-4 border-l-4 rounded-tl-lg rounded-bl-lg ${colorB}`}
               >
-                {song.rooms}
+                <p className={ps}>{song.rooms}</p>
               </td>
-              <td className="px-6 py-3 border-t border-b border-red-600 ">
-                {song.surface} m2
+              <td className={`px-6 py-3 border-t-4 border-b-4 ${colorB}`}>
+                <p className={ps}>{song.surface} m2</p>
               </td>
-              <td className="px-6 py-3 border-t border-b border-red-600 ">
-                {song.songAData.map((data, subIndex) => (
-                  <div key={subIndex} className="flex-col">
-                    <p>{data.bien}</p>
-                    <p>{data.Pabien}</p>
-                  </div>
-                ))}
+              <td className={`px-6 py-3 border-t-4 border-b-4 ${colorB}`}>
+                <p className={ps}>{song.prestations}</p>
               </td>
-              <td className="px-6 py-3 border-t border-b border-red-600 ">
-                {song.level}
+              <td className={`px-6 py-3 border-t-4 border-b-4 ${colorB}`}>
+                <p className={ps}>{song.level}</p>
               </td>
-              <td className="px-6 py-3 border-t border-b border-red-600 ">
-                {song.Price}
+              <td className={`px-6 py-3 border-t-4 border-b-4 ${colorB}`}>
+                <p className={ps}>{song.Price}</p>
               </td>
-              <td className="px-6 py-3 border-t border-b rounded-tr-lg rounded-br-lg border-r border-red-600 ">
-                <Pop Price={song.Price} />
+              <td
+                className={`px-6 py-3 border-t-4 border-b-4 rounded-tr-lg rounded-br-lg border-r-4 ${colorB}`}
+              >
+                <Pop
+                  Price={song.Price}
+                  DateDispo={song.songAData.DateDispo}
+                  City={song.songAData.City}
+                  Residence={song.songAData.Residence}
+                  PriceP={song.songAData.PriceP}
+                  Rooms={song.songAData.rooms}
+                  surface={song.songAData.surface}
+                  Ann1={song.songAData.Ann1}
+                  Ann2={song.songAData.Ann2}
+                  Ann3={song.songAData.Ann3}
+                  Ann4={song.songAData.Ann4}
+                  Ann5={song.songAData.Ann5}
+                  tAnn1={song.songAData.tAnn1}
+                  tAnn2={song.songAData.tAnn2}
+                  tAnn3={song.songAData.tAnn3}
+                  tAnn4={song.songAData.tAnn4}
+                  tAnn5={song.songAData.tAnn5}
+                  refA={song.songAData.ref}
+                />
               </td>
             </tr>
           ))}
