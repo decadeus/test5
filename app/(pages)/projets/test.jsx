96a570b578b677accbc6ed7b169ff2f55208592a
decadeus <debeaumont@decadeus.com>
2024-07-17 03:43:44 +0200

    projets

--- app/(pages)/projets/test.jsx
+++ app/(pages)/projets/test.jsx
@@ -1,4 +1,5 @@
 "use client";
+import React, { useState, useEffect } from "react";
 import {
   Modal,
   ModalContent,
@@ -6,23 +7,22 @@ import {
   Button,
   useDisclosure,
 } from "@nextui-org/react";
-import React from "react";
 import { FaPlusCircle } from "react-icons/fa";
 import Image from "next/image";
 import Im from "@/components/image/appart3.jpg";
 import archi from "@/components/image/plan_archi.jpg";
-
+import { MdDateRange } from "react-icons/md";
 
 export default function Test() {
-  // Define a constant map with song data
   const songData = [
     {
-      rooms: 3,
-      surface: 50,
-      prestations: 1961,
-      level: "RDC",
-      Price: "15 000 Pln",
-      Detail: "Details",
+      ref: "504E",
+      bedrooms: 2,
+      bathrooms: 2,
+      floor: 4,
+      surface: 70,
+      price: "zł 100 000",
+      available: "12/09/2027",
       songAData: {
         ref: "ref: 233N4BB",
         DateDispo: "Livraison prevu pour le 12/09/2025",
@@ -30,7 +30,7 @@ export default function Test() {
         Residence: "Residence name",
         PriceP: "230 000 Pln",
         rooms: "3 pieces",
-        surface: "54 m2",
+        surface: "59 m2",
         Ann1: "Ann1",
         Ann2: "Ann2",
         Ann3: "Ann3",
@@ -41,23 +41,26 @@ export default function Test() {
         tAnn3: "dAnn3",
         tAnn4: "dAnn4",
         tAnn5: "dAnn5",
+        LongTest:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, non dolorem illo aperiam debitis, reiciendis facilis neque facere repellendus aut sequi laudantium odio natus ipsam ratione, sed ex quas accusantium.",
       },
     },
     {
-      rooms: 2,
-      surface: 100,
-      prestations: 1961,
-      level: 2,
-      Price: "1000",
-      Detail: "Details",
+      ref: "504E",
+      bedrooms: 3,
+      bathrooms: 2,
+      floor: 4,
+      surface: 50,
+      available: "12/09/2025",
+      price: "zł 150 000",
       songAData: {
-        ref: "ref",
+        ref: "ref: 233N4BB",
         DateDispo: "Livraison prevu pour le 12/09/2025",
         City: "Warsaw",
         Residence: "Residence name",
         PriceP: "230 000 Pln",
         rooms: "3 pieces",
-        surface: "54 m2",
+        surface: "59 m2",
         Ann1: "Ann1",
         Ann2: "Ann2",
         Ann3: "Ann3",
@@ -68,23 +71,26 @@ export default function Test() {
         tAnn3: "dAnn3",
         tAnn4: "dAnn4",
         tAnn5: "dAnn5",
+        LongTest:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, non dolorem illo aperiam debitis, reiciendis facilis neque facere repellendus aut sequi laudantium odio natus ipsam ratione, sed ex quas accusantium.",
       },
     },
     {
-      rooms: 4,
-      surface: 120,
-      prestations: 1961,
-      level: 20,
-      Price: "1000",
-      Detail: "Details",
+      ref: "504E",
+      bedrooms: 3,
+      bathrooms: 2,
+      floor: 4,
+      surface: 50,
+      available: "12/09/2025",
+      price: "zł 150 000",
       songAData: {
-        ref: "ref",
+        ref: "ref: 233N4BB",
         DateDispo: "Livraison prevu pour le 12/09/2025",
         City: "Warsaw",
         Residence: "Residence name",
         PriceP: "230 000 Pln",
         rooms: "3 pieces",
-        surface: "54 m2",
+        surface: "59 m2",
         Ann1: "Ann1",
         Ann2: "Ann2",
         Ann3: "Ann3",
@@ -95,23 +101,26 @@ export default function Test() {
         tAnn3: "dAnn3",
         tAnn4: "dAnn4",
         tAnn5: "dAnn5",
+        LongTest:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, non dolorem illo aperiam debitis, reiciendis facilis neque facere repellendus aut sequi laudantium odio natus ipsam ratione, sed ex quas accusantium.",
       },
     },
     {
-      rooms: 3,
-      surface: 90,
-      prestations: 1961,
-      level: 4,
-      Price: "1000",
-      Detail: "Details",
+      ref: "504E",
+      bedrooms: 3,
+      bathrooms: 2,
+      floor: 4,
+      surface: 50,
+      price: "zł 150 000",
+      available: "12/09/2025",
       songAData: {
-        refA: "ref",
-        DateDispo: "DateDispo",
-        City: "City",
-        Residence: "Residence",
-        PriceP: "Price",
-        Rooms: "Rooms",
-        surface: "surface",
+        ref: "ref: 233N4BB",
+        DateDispo: "Livraison prevu pour le 12/09/2025",
+        City: "Warsaw",
+        Residence: "Residence name",
+        PriceP: "230 000 Pln",
+        rooms: "3 pieces",
+        surface: "59 m2",
         Ann1: "Ann1",
         Ann2: "Ann2",
         Ann3: "Ann3",
@@ -122,18 +131,18 @@ export default function Test() {
         tAnn3: "dAnn3",
         tAnn4: "dAnn4",
         tAnn5: "dAnn5",
+        LongTest:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, non dolorem illo aperiam debitis, reiciendis facilis neque facere repellendus aut sequi laudantium odio natus ipsam ratione, sed ex quas accusantium.",
       },
     },
   ];
 
   return (
     <div className="w-full flex flex-col justify-center items-center gap-16">
-        <div>
-      <Appartement songs={songData} />
-      </div>
       <div>
-      <button className=" bg-red-600 rounded-xl px-4 py-2 text-white text-xl shadow-2xl">Nous contacter</button>
+        <Appartement songs={songData} />
       </div>
+     
     </div>
   );
 }
@@ -142,7 +151,7 @@ function Appartement({ songs }) {
   return (
     <div className="w-full flex-col flex gap-16">
       <div
-        className="relative overflow-hidden  bg-cover bg-no-repeat w-full"
+        className="relative overflow-hidden bg-cover bg-no-repeat w-full"
         style={{
           backgroundImage: `url(${Im.src})`,
           height: "400px",
@@ -189,199 +198,150 @@ function Appartement({ songs }) {
           </div>
         </div>
       </div>
-      <div className="pt-16">
-        <Tab songs={songs} />
+      <div className="w-full justify-center flex">
+        <div className="pt-32  w-fit ">
+          <Tab2 songs={songs} />
+        </div>
       </div>
     </div>
   );
 }
+function Tab2({ songs }) {
+  const [selectedSong, setSelectedSong] = useState(null);
 
-function Pop({
-  Price,
-  DateDispo,
-  City,
-  Residence,
-  PriceP,
-  Rooms,
-  surface,
-  Ann1,
-  Ann2,
-  Ann3,
-  Ann4,
-  Ann5,
-  tAnn1,
-  tAnn2,
-  tAnn3,
-  tAnn4,
-  tAnn5,
-  refA,
-}) {
-  const { isOpen, onOpen, onOpenChange } = useDisclosure();
-  return (
-    <>
-      <Button
-        onPress={onOpen}
-        isIconOnly
-        className="bg-transparent rounded-full  hover:rotate-45"
-      >
-        <FaPlusCircle color="indigo" size={30}  />
-      </Button>
-      <Modal
-        isOpen={isOpen}
-        onOpenChange={onOpenChange}
-        size="4xl"
-        placement="top"
-        className="px-0 py-0"
-      >
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalBody className="w-full py-8 px-8">
-                <div className="bg-gradient-to-br from-sky-900 to-blue-400 text-white w-full p-4 rounded-xl ">
-                  <div className="flex justify-between items-center">
-                    
-                    <div className="flex items-center gap-16">
-                      <p className="text-2xl font-bold">{Price}</p>
-                      <p>{DateDispo}</p>
-                    </div>
-                    <div>
-                      <p>{refA}</p>
-                    </div>
-                  </div>
-                  <p className="text-2x font-semibold">{City}</p>
-                  <p className="text-xl font-semibold">{Residence}</p>
+  useEffect(() => {
+    // Automatically select the first song on load
+    if (songs.length > 0) {
+      setSelectedSong(songs[0]);
+    }
+  }, [songs]);
 
-                  <div className="flex gap-4">
-                    <p>{Rooms}</p> <p>{surface}</p>
-                  </div>
-                </div>
-                <div className="w-full ">
-                  <Image src={archi} alt="appart" />
+  const handleRowClick = (song) => {
+    setSelectedSong(song);
+  };
 
-                  <div className="flex gap-2">
-                    <div className="font-bold">
-                      <p>{Ann1}</p>
-                      <p>{Ann2}</p>
-                      <p>{Ann3}</p>
-                      <p>{Ann4}</p>
-                      <p>{Ann5}</p>
-                    </div>
-                    <div className="">
-                      <p>{tAnn1}</p>
-                      <p>{tAnn2}</p>
-                      <p>{tAnn3}</p>
-                      <p>{tAnn4}</p>
-                      <p>{tAnn5}</p>
-                    </div>
-                  </div>
-                </div>
-              </ModalBody>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
-    </>
-  );
-}
-
-function Tab({ songs }) {
   const colorB = "border-violet-900";
-  const ps = "text-black font-bold";
+  const ps = "text-white font-bold text-center py-2";
+  const Tt = "text-gray-500 font-extra-bold text-center px-2";
+
   return (
-    <div className="overflow-x-auto bg-white md:px-64">
-      <table
-        className="min-w-full text-left text-xs whitespace-nowrap border-separate"
-        style={{ borderSpacing: "0 10px" }}
-      >
-        <thead className="uppercase tracking-wider bg-neutral-50">
-          <tr>
-            <th
-              scope="col"
-              className="px-6 py-3 border-b border-t border-gray-300"
-            >
-              Rooms
-            </th>
-            <th
-              scope="col"
-              className="px-6 py-3 border-b border-t border-gray-300"
-            >
-              Surface
-            </th>
-            <th
-              scope="col"
-              className="px-6 py-3 border-b border-t border-gray-300"
-            >
-              Prestations
-            </th>
-            <th
-              scope="col"
-              className="px-6 py-3 border-b border-t border-gray-300"
-            >
-              Level
-            </th>
-            <th
-              scope="col"
-              className="px-6 py-3 border-b border-t border-gray-300"
-            >
-              Price
-            </th>
-            <th
-              scope="col"
-              className="px-6 py-3 border-b border-t border-gray-300"
-            >
-              Detail
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {songs.map((song, index) => (
-            <tr key={index} className="group hover:bg-violet-200">
-              <td
-                scope="row"
-                className={`px-6 py-3 border-t-4 border-b-4 border-l-4 rounded-tl-lg rounded-bl-lg ${colorB}`}
-              >
-                <p className={ps}>{song.rooms}</p>
-              </td>
-              <td className={`px-6 py-3 border-t-4 border-b-4 ${colorB}`}>
-                <p className={ps}>{song.surface} m2</p>
-              </td>
-              <td className={`px-6 py-3 border-t-4 border-b-4 ${colorB}`}>
-                <p className={ps}>{song.prestations}</p>
-              </td>
-              <td className={`px-6 py-3 border-t-4 border-b-4 ${colorB}`}>
-                <p className={ps}>{song.level}</p>
-              </td>
-              <td className={`px-6 py-3 border-t-4 border-b-4 ${colorB}`}>
-                <p className={ps}>{song.Price}</p>
-              </td>
-              <td
-                className={`px-6 py-3 border-t-4 border-b-4 rounded-tr-lg rounded-br-lg border-r-4 ${colorB}`}
-              >
-                <Pop
-                  Price={song.Price}
-                  DateDispo={song.songAData.DateDispo}
-                  City={song.songAData.City}
-                  Residence={song.songAData.Residence}
-                  PriceP={song.songAData.PriceP}
-                  Rooms={song.songAData.rooms}
-                  surface={song.songAData.surface}
-                  Ann1={song.songAData.Ann1}
-                  Ann2={song.songAData.Ann2}
-                  Ann3={song.songAData.Ann3}
-                  Ann4={song.songAData.Ann4}
-                  Ann5={song.songAData.Ann5}
-                  tAnn1={song.songAData.tAnn1}
-                  tAnn2={song.songAData.tAnn2}
-                  tAnn3={song.songAData.tAnn3}
-                  tAnn4={song.songAData.tAnn4}
-                  tAnn5={song.songAData.tAnn5}
-                  refA={song.songAData.ref}
-                />
-              </td>
+    <div className="overflow-x-auto bg-gray-700 flex w-full">
+      <div className="w-1/2 px-8">
+        <table
+          className="min-w-full text-left text-xs whitespace-nowrap border-separate pt-4"
+          style={{ borderSpacing: "0 10px" }}
+        >
+          <thead className="uppercase tracking-wider">
+            <tr>
+              <th scope="col" className={Tt}>
+                REF
+              </th>
+              <th scope="col" className={Tt}>
+                BED
+              </th>
+              <th scope="col" className={Tt}>
+                BATH
+              </th>
+              <th scope="col" className={Tt}>
+                FLOOR
+              </th>
+              <th scope="col" className={Tt}>
+                SURFACE (m2)
+              </th>
+              <th scope="col" className={Tt}>
+                AVAILABLE
+              </th>
+              <th scope="col" className={Tt}>
+                Price
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {songs.map((song, index) => (
+              <tr
+                key={index}
+                className={`group cursor-pointer  ${
+                  selectedSong === song ? "bg-gray-900" : "hover:bg-gray-800"
+                }`}
+                onClick={() => handleRowClick(song)}
+              >
+                <td scope="row" className="text-center">
+                  <p className={ps}>{song.ref}</p>
+                </td>
+                <td scope="row" className="text-center">
+                  <p className={ps}>{song.bedrooms}</p>
+                </td>
+                <td className="">
+                  <p className={ps}>{song.bathrooms}</p>
+                </td>
+                <td className="">
+                  <p className={ps}>{song.floor}</p>
+                </td>
+
+                <td className="">
+                  <p className={ps}>{song.surface}</p>
+                </td>
+                <td className="">
+                  <p className={ps}>{song.available}</p>
+                </td>
+                <td className="">
+                  <p className={ps}>{song.price}</p>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="bg-gray-900 w-1/2 p-4">
+     
+        
+       
+
+        <div className="w-full">
+        <div className="relative w-full h-[300px] ">
+            <Image src={Im} alt="appart" layout="fill"
+          objectFit="cover"/>
+          </div>
+          {selectedSong ? (
+            <div className="pt-4">
+              <div className={` flex justify-between text-stone-400`}>
+                <div className="flex gap2">
+                  <p className="border-r-2 pr-2">Price: {selectedSong.price}</p>
+                  <p className="border-r-2 pr-2 pl-2">
+                    Bedrooms: {selectedSong.bedrooms}
+                  </p>
+                  <p className="pl-2">Surface: {selectedSong.surface} m2</p>
+                </div>
+                <div>
+                  <p className="pr-2 flex justify-center items-center gap-2">
+                    <MdDateRange />
+                    available: {selectedSong.available}
+                  </p>
+                </div>
+              </div>
+              <p className="text-sm text-white">
+                Ref: {selectedSong.songAData.LongTest}
+              </p>
+              <p className="text-sm text-white">
+                Ref: {selectedSong.songAData.LongTest}
+              </p>
+              <p className="text-sm text-white">
+                Ref: {selectedSong.songAData.LongTest}
+              </p>
+              <p className="text-sm text-white">
+                Ref: {selectedSong.songAData.LongTest}
+              </p>
+              <div className="pt-4">
+                <button className="bg-emerald-500 rounded-xl px-4 py-2 text-white text-normal shadow-2xl">
+                  Plus d'information
+                </button>
+              </div>
+            </div>
+          ) : (
+            <p>Click on a row to see more details</p>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
