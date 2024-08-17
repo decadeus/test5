f164a43e752155d9ae93b92d72cfec937092d5a0
decadeus <debeaumont@decadeus.com>
2024-08-17 17:01:52 +0200

    test

--- components/fullmap.jsx
+++ components/fullmap.jsx
@@ -1,89 +1,89 @@
-"use client";
+// "use client";
 
-import React, { useState, useEffect } from "react";
-import "leaflet/dist/leaflet.css";
-import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css";
-import "leaflet-defaulticon-compatibility";
-
-
-import Avatar from "@/app/getimage/Ugetone";
-import Link from "next/link";
+// import React, { useState, useEffect } from "react";
+// import "leaflet/dist/leaflet.css";
+// import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css";
+// import "leaflet-defaulticon-compatibility";
 
 
+// import Avatar from "@/app/getimage/Ugetone";
+// import Link from "next/link";
 
 
-// Dynamic import of react-leaflet components
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
-const getMapBounds = (todos) => {
-  // Initialize empty bounds
-  const bounds = new L.latLngBounds([]);
 
-  // Iterate through todos and extend bounds for each marker
-  todos.forEach((todo) => {
-    const latLng = new L.latLng(todo.lat, todo.lng);
-    bounds.extend(latLng);
-  });
+// // Dynamic import of react-leaflet components
+// import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
-  return bounds;
-};
+// const getMapBounds = (todos) => {
+//   // Initialize empty bounds
+//   const bounds = new L.latLngBounds([]);
 
-const MapComponent = ({ classN, todos }) => {
-  const [center, setCenter] = useState([52.07957, 20.97848]); // Default center (Continental United States)
+//   // Iterate through todos and extend bounds for each marker
+//   todos.forEach((todo) => {
+//     const latLng = new L.latLng(todo.lat, todo.lng);
+//     bounds.extend(latLng);
+//   });
 
-  useEffect(() => {
-    if (todos.length > 0) {
+//   return bounds;
+// };
 
-      const bounds = getMapBounds(todos);
+// const MapComponent = ({ classN, todos }) => {
+//   const [center, setCenter] = useState([52.07957, 20.97848]); // Default center (Continental United States)
+
+//   useEffect(() => {
+//     if (todos.length > 0) {
+
+//       const bounds = getMapBounds(todos);
       
 
-      // Automatically adjusts zoom to fit bounds
-      setCenter(bounds.getCenter());
-    }
-  }, [todos]);
+//       // Automatically adjusts zoom to fit bounds
+//       setCenter(bounds.getCenter());
+//     }
+//   }, [todos]);
 
-  return (
-    <MapContainer center={center} zoom={4} className={classN}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      />
-      {todos
-        .filter(todo => todo.lat && todo.lng) // Ensure lat and lng are defined
-        .map((todo) => (
-          <Marker
-            key={todo.id}
-            position={[todo.lat, todo.lng]}
-            style={{ color: "lightblue" }}
+//   return (
+//     <MapContainer center={center} zoom={4} className={classN}>
+//       <TileLayer
+//         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//       />
+//       {todos
+//         .filter(todo => todo.lat && todo.lng) // Ensure lat and lng are defined
+//         .map((todo) => (
+//           <Marker
+//             key={todo.id}
+//             position={[todo.lat, todo.lng]}
+//             style={{ color: "lightblue" }}
            
           
            
-          >
-            <Popup maxWidth={500}>
-              <div className="flex-row w-full">
-                <div className="w-[150px] aspect-square rounded-md">
-                  <Avatar
-                    url={todo.mainpic_url}
-                    width={350}
-                    height={350}
-                    className="w-[350px] h-[350px] rounded-md"
-                    style={{
-                      objectFit: "cover",
-                      width: "350px",
-                      height: "350px",
-                    }}
-                  />
-                </div>
-                <div className="pt-2">{todo.city}</div>
-                <Link href={`/${todo.id}`}>
-                  <p className="font-bold">See</p>
-                </Link>
-              </div>
-            </Popup>
-          </Marker>
-        ))}
-    </MapContainer>
-  );
-};
+//           >
+//             <Popup maxWidth={500}>
+//               <div className="flex-row w-full">
+//                 <div className="w-[150px] aspect-square rounded-md">
+//                   <Avatar
+//                     url={todo.mainpic_url}
+//                     width={350}
+//                     height={350}
+//                     className="w-[350px] h-[350px] rounded-md"
+//                     style={{
+//                       objectFit: "cover",
+//                       width: "350px",
+//                       height: "350px",
+//                     }}
+//                   />
+//                 </div>
+//                 <div className="pt-2">{todo.city}</div>
+//                 <Link href={`/${todo.id}`}>
+//                   <p className="font-bold">See</p>
+//                 </Link>
+//               </div>
+//             </Popup>
+//           </Marker>
+//         ))}
+//     </MapContainer>
+//   );
+// };
 
-export default MapComponent;
+// export default MapComponent;
