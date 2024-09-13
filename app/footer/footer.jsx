ba80d99afc836263059ecb1a00b5760e20c5de43
decadeus <debeaumont@decadeus.com>
2024-09-13 16:29:58 +0200

    footer

--- app/footer/footer.jsx
+++ app/footer/footer.jsx
@@ -5,8 +5,8 @@ import { FaTwitter } from "react-icons/fa";
 
 export default function Foot() {
   return (
-    <footer className="bg-gray-300 flex py-16 border-t-1 border-gray-500 xl:px-24 px-2  gap-10 ">
-      <div className="flex gap-2 justify-end items-end border-r-1 border-black pr-4">
+    <footer className="bg-gray-300 flex py-4 border-t-1 border-gray-500 xl:px-12 px-4  gap-10 ">
+      <div className="flex flex-col gap-2 justify-start items-start border-r-1 border-black pr-4 ">
         <div className="w-16 h-16 rounded-full">
           <Image
             src={b}
@@ -16,23 +16,25 @@ export default function Foot() {
             className="rounded-full"
           />
         </div>
-        <div className="flex flex-col justify-end items-end">
-          <div>
-            <p className="text-2xl font-bold">Hoomge.com</p>
-          </div>
-          <div>
-            <p className="text-md">© 2024 Decadeus</p>
-          </div>
-          <div className="border border-black rounded-full p-1">
-          <FaTwitter />
-          </div>
 
-          
+        <div>
+          <p className="text-md">© 2024 Decadeus</p>
+        </div>
+        <div className="flex flex-col justify-end items-end"></div>
+      </div>
+      <div className="flex-col content-between border-black pr-4 border-r-1 justify-center itenms-center">
+        <div>
+          <p className="text-2xl font-bold">Hoomge.com</p>
+        </div>
+        <div>
+          <p>Terms and Conditions</p>
+          <p>Add a Property Terms of Service</p>
         </div>
       </div>
-      <div>
-        <p>Terms and Conditions</p>
-        <p>Add a Property Terms of Service</p>
+      <div className="flex-col content-between ">
+        <div className="border border-black rounded-full p-1 w-fit">
+          <FaTwitter />
+        </div>
       </div>
     </footer>
   );
