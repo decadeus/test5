92e5b15d0b2d7c917520ca892558b52fd6eb94e0
decadeus <debeaumont@decadeus.com>
2024-09-25 17:18:52 +0200

    mainpage

--- app/page.jsx
+++ app/page.jsx
@@ -1,21 +1,21 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import Form from "@/app/addproject/form";
+import { useState, useEffect } from "react"; // Ajout de useState et useEffect
 import d from "@/components/d.png";
 import e from "@/components/e.png";
 import Image from "next/legacy/image";
-import { FaArrowRight } from "react-icons/fa";
-import { FaHeart } from "react-icons/fa";
+import { FaArrowRight, FaHeart, FaLongArrowAltDown } from "react-icons/fa";
 import { createClient } from "@/utils/supabase/client";
 import Avatar from "@/app/getimage/project";
 import * as ScrollArea from "@radix-ui/react-scroll-area";
 import { motion } from "framer-motion";
-import { FaLongArrowAltDown } from "react-icons/fa";
+import {texts} from "@/lib/language"; 
 
 export default function Page() {
   const [projects, setProjects] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [selectedCountry, setSelectedCountry] = useState("France");
+  const [language, setLanguage] = useState("fr");
 
   const containerStyle = "flex flex-col items-center bgfull w-full pt-32 pb-32";
   const headerStyle =
@@ -24,7 +24,10 @@ export default function Page() {
 
   const fetchProjects = async () => {
     const supabase = createClient();
-    const { data, error } = await supabase.from("project").select("*");
+    const { data, error } = await supabase
+      .from("project")
+      .select("*")
+      .eq("country", selectedCountry);
 
     if (error) {
       setError(error);
@@ -36,6 +39,14 @@ export default function Page() {
 
   useEffect(() => {
     fetchProjects();
+  }, [selectedCountry]); // Re-fetch projects when selectedCountry changes
+
+  const handleCountryChange = (country) => {
+    setSelectedCountry(country);
+    setLanguage(country === "Polska" ? "pl" : "fr"); // Change la langue en fonction du pays
+  };
+
+  useEffect(() => {
     const revealText = (id, text, buildingText) => {
       const container = document.getElementById(id);
       container.innerHTML = "";
@@ -49,6 +60,7 @@ export default function Page() {
         span.textContent = char === " " ? "\u00A0" : char;
         container.appendChild(span);
       }
+
       const buildingSpan = document.createElement("span");
       buildingSpan.className =
         "bg-gradient-to-r from-fuchsia-400 via-pink-500 to-sky-500 bg-clip-text text-transparent";
@@ -72,78 +84,65 @@ export default function Page() {
 
       window.addEventListener("scroll", revealLetters);
     };
-    revealText("building-text", "Fill information about ", "the building");
+
     revealText(
       "second-title-text",
       "Discover amazing features of ",
       "this project"
     );
-    revealText("third-title-text", "Follow all new ", "projects");
   }, []);
+
+
 
   return (
     <>
       <style jsx>{`
         .letter {
-          display: inline-block; /* Pour pouvoir les révéler individuellement */
-          opacity: 0; /* Invisible au départ */
-          transition: opacity 0.3s ease; /* Transition douce */
+          display: inline-block;
+          opacity: 0;
+          transition: opacity 0.3s ease;
         }
       `}</style>
-      <div className="w-full">
-        <Para />
+      <div className="w-full maintextfull">
+        <Para language={language} texts={texts} />
       </div>
-      <div className={containerStyle}>
-        <h1 className={headerStyle}>
-          The Largest Collection of <br /> Residential Real Estate Projects
-        </h1>
-        <p className={subheaderStyle}>
-          List Your Residential Apartments for Sale in Minutes, Completely Free.
-        </p>
-        <button className="bg-blue-500 text-white py-2 px-4 text-xl mb-16 font-satisfy">
-          Add your project
+
+      <div className="flex justify-center mb-8 -mt-32 z-50">
+        <button
+          onClick={() => handleCountryChange("France")}
+          className={`flex justify-between items-center  gap-2 px-4 py-2 m-2  borderfull border-2 rounded ${
+            selectedCountry === "France"
+              ? "mainbgfull text-white"
+              : "bg-white colortext"
+          }`}
+        >
+         <img src="/france-flag-round-circle-icon.svg" alt="Icon" width="20" height="20" /> France
         </button>
-        <Step />
+        <button
+          onClick={() => handleCountryChange("Polska")}
+          className={`flex justify-between items-center  gap-2 px-4 py-2 m-2  borderfull border-2 rounded ${
+            selectedCountry === "Polska"
+              ? "mainbgfull text-white"
+              : "bg-white colortext"
+          }`}
+        >
+          <img src="/monaco-flag-round-circle-icon.svg" alt="Icon" width="20" height="20" /> Polska
+        </button>
+      </div>
+
+      <div className={containerStyle}>
         <div className="flex flex-col justify-start items-start w-full mb-16">
           <p className="text-black mb-4">Updates</p>
           <h2 className="text-black text-5xl font-bold mb-8">
-            <span id="third-title-text">
-              {/* Le contenu sera ajouté via JavaScript */}
-            </span>
+            <span id="third-title-text"></span>
           </h2>
-          <p className="text-gray-400 text-xl mb-20 font-kenia">
-            Stay informed about our latest developments and offerings.
-          </p>
         </div>
 
         <div className="flex flex-col justify-start items-start w-full mb-16">
-          <p className="text-black mb-4">Building</p>
-          <h2 className="text-black text-5xl font-bold mb-8">
-            <span id="building-text">
-              {/* Le contenu sera ajouté via JavaScript */}
-            </span>
-          </h2>
-          <p className="text-gray-400 text-xl mb-20">
-            Present the advantages of the residence by highlighting and
-            selecting specific features to facilitate the apartment search. Once
-            all the information is provided, you can immediately list your
-            available apartments for sale online.
-          </p>
-          <div className="w-[450px] shadowW">
-            <Image src={d} width={450} height={450} alt="Logo" className="" />
-          </div>
-        </div>
-
-        <div className="flex flex-col justify-start items-start w-full mb-16">
-          <p className="text-black mb-4">Features</p>
           <h2 className="text-black text-5xl font-bold mb-8 text-wrap">
-            <span id="second-title-text">
-              {/* Le contenu sera ajouté via JavaScript */}
-            </span>
+            <span id="second-title-text"></span>
           </h2>
-          <p className="font-kenia">
-            Explore the unique characteristics that make this project stand out.
-          </p>
+
           <div className="flex items-center justify-center ">
             <p className="flex items-center text-6xl">
               We{" "}
@@ -164,161 +163,27 @@ export default function Page() {
           </div>
           <Black projects={projects} />
         </div>
-
-        <Form />
       </div>
-      <div className="w-full flex flex-col justify-center bg-[#18191C] py-8">
-        <h2 className="font-macondo text-white text-4xl text-center">
-          CECI EST UN TEST
+
+      <div className="w-full flex flex-col justify-center py-8">
+        <h2 className="font-macondo text-black text-4xl text-center">
+          {texts[language].title} {/* Utiliser le texte basé sur la langue */}
         </h2>
 
-        <Scroll projects={projects.filter(p => [10, 12, 13, 14].includes(p.id))} />
+        <Scroll
+          projects={projects.filter((p) => [10, 12, 13, 14].includes(p.id))}
+        />
       </div>
 
-      <div className="w-full flex flex-col bg-[#18191C] py-8">
-      <h2 className="font-macondo text-white text-4xl text-center pb-8">
-          CECI EST UN TEST
+      <div className="w-full flex flex-col bg-[#18191C]  py-8 mb-4">
+        <h2 className="font-macondo maintextfull text-4xl text-center pb-8 ">
+          {texts[language].title} {/* Utiliser le texte basé sur la langue */}
         </h2>
         {projects
-          .filter((p) => [10, 12, 14].includes(p.id))
+          .filter((p) => [10, 12,13, 14].includes(p.id))
           .map((project, index) => (
-            <Demi key={project.id} projects={project} index={index} />
+            <Demi key={project.id} projects={project} index={index} texts={texts} language={language} />
           ))}
-      </div>
-      
-
-    </>
-  );
-}
-function Step() {
-  const stepContainerStyle = [
-    {
-      step: "1",
-      title: "Fill the form",
-      para: "Fill in the information about the building and the apartments",
-    },
-    {
-      step: "2",
-      title: "Template 2",
-      para: "Fill in the information about the building and the apartments",
-    },
-    {
-      step: "3",
-      title: "Template 3",
-      para: "Fill in the information about the building and the apartments",
-    },
-    {
-      step: "4",
-      title: "Template 4",
-      para: "Fill in the information about the building and the apartments",
-    },
-  ];
-
-  const scrollToSection = () => {
-    const section = document.getElementById("ici");
-    if (section) {
-      section.scrollIntoView({ behavior: "smooth" });
-    }
-  };
-
-  return (
-    <>
-      <div className="flex gap-4 mb-12 w-full">
-        {stepContainerStyle.map(({ step, title, para }) => (
-          <div
-            key={step}
-            className="relative text-white bg-gray-800 hover:bg-black p-4 rounded-bl-2xl rounded-tl-2xl rounded-br-2xl hover:-translate-y-4 transition-transform duration-300 ease-in-out group"
-          >
-            <div className="absolute top-[20px] left-1/4 transform -translate-x-1/2 transition-transform duration-1000 ease-in-out group-hover:rotate-180">
-              <svg
-                width="70"
-                height="70"
-                viewBox="0 0 200 200"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <line
-                  x1="100"
-                  y1="100"
-                  x2="100"
-                  y2="10"
-                  stroke="white"
-                  strokeWidth="4"
-                />
-                <line
-                  x1="100"
-                  y1="100"
-                  x2="164.64"
-                  y2="35.36"
-                  stroke="white"
-                  strokeWidth="12"
-                />
-                <line
-                  x1="100"
-                  y1="100"
-                  x2="190"
-                  y2="100"
-                  stroke="white"
-                  strokeWidth="4"
-                />
-                <line
-                  x1="100"
-                  y1="100"
-                  x2="164.64"
-                  y2="164.64"
-                  stroke="white"
-                  strokeWidth="12"
-                />
-                <line
-                  x1="100"
-                  y1="100"
-                  x2="100"
-                  y2="190"
-                  stroke="white"
-                  strokeWidth="4"
-                />
-                <line
-                  x1="100"
-                  y1="100"
-                  x2="35.36"
-                  y2="164.64"
-                  stroke="white"
-                  strokeWidth="12"
-                />
-                <line
-                  x1="100"
-                  y1="100"
-                  x2="10"
-                  y2="100"
-                  stroke="white"
-                  strokeWidth="4"
-                />
-                <line
-                  x1="100"
-                  y1="100"
-                  x2="35.36"
-                  y2="35.36"
-                  stroke="white"
-                  strokeWidth="12"
-                />
-                <circle cx="100" cy="100" r="8" fill="white" />
-              </svg>
-            </div>
-
-            <p className="colortext mt-24">STEP {step}</p>
-            <h1 className="text-2xl mt-4">{title}</h1>
-            <p className="mt-4">{para}</p>
-            <div className="my-4">
-              <hr className="border-t border-gray-300" />
-            </div>
-            <button
-              onClick={scrollToSection}
-              className="flex items-center text-white cursor-pointer colortext group"
-            >
-              GET STARTED
-              <FaArrowRight className="ml-2 transition-transform duration-300 ease-in-out group-hover:rotate-90" />
-            </button>
-          </div>
-        ))}
       </div>
     </>
   );
@@ -340,7 +205,7 @@ function Black({ projects }) {
           {/* Bloc 2: 6/12 */}
           <div className="relative h-72 w-6/12 flex flex-col text-center items-center">
             <div>
-              <p className="text-white pr-12 flex text-center text-5xl pb-4 font-satisfy colortext">
+              <p className="maintextfull pr-12 flex text-center text-5xl pb-4 font-satisfy colortext">
                 {item.name}
               </p>
             </div>
@@ -356,7 +221,7 @@ function Black({ projects }) {
 
           {/* Bloc 3: 3/12 */}
           <div className="w-3/12 pl-4">
-            <p className="text-white pr-12 flex items-center text-base font-thin">
+            <p className="maintextfull pr-12 flex items-center text-base font-thin">
               {item.Des}
             </p>
           </div>
@@ -424,14 +289,14 @@ function Scroll({ projects = [] }) {
                     />
                   </div>
                   <div className="mt-4 ">
-                    <p className="text-white colortext font-satisfy text-2xl font-extrabold ">
+                    <p className="maintextfull colortext font-satisfy text-2xl font-extrabold ">
                       {item.name}
                     </p>
-                    <p className="text-white">{item.adresse}</p>
-                    <p className="text-white">
+                    <p className="maintextfull">{item.adresse}</p>
+                    <p className="maintextfull">
                       {item.city}, {item.country}
                     </p>
-                    <p className="text-white">{item.compagny}</p>
+                    <p className="maintextfull">{item.compagny}</p>
                   </div>
                 </a>
               </div>
@@ -482,9 +347,7 @@ function Scroll({ projects = [] }) {
   );
 }
 
-// CSS styles remain the same...
-
-function Demi({ projects, index }) {
+function Demi({ projects, index ,texts, language }) {
   // Vérifier si l'index est pair ou impair pour changer l'ordre des blocs
   const isEven = index % 2 === 0;
 
@@ -505,30 +368,18 @@ function Demi({ projects, index }) {
 
       {/* Bloc B - Texte */}
       <div className="w-1/2 flex flex-col bg-[#18191C] justify-center items-center">
-        <p className="text-white colortext font-satisfy text-2xl font-extrabold">
-          {projects.name || "Nom du projet non disponible"}
+        <p className="maintextfull colortext font-satisfy text-4xl font-extrabold pb-8">
+          {projects.name}
         </p>
-        <p className="text-white">
-          {projects.adresse || "Adresse non disponible"}
-        </p>
-        <p className="text-white">
-          {projects.city
-            ? `${projects.city}, ${projects.country}`
-            : "Ville et pays non disponibles"}
-        </p>
-        <p className="text-white">
-          {projects.compagny || "Compagnie non disponible"}
-        </p>
+        <p className="maintextfull px-12 pb-8">{projects.Des}</p>
+
         {projects.link && (
-          <a
-            href={projects.link}
-            target="_blank"
-            rel="noopener noreferrer"
+          <a href={projects.link} target="_blank" rel="noopener noreferrer">
             
-          >
-            <p className="colortext">
-              Learn more about {projects.name || "ce projet"}
+            <p className="colortext hover:underline">
+              {texts[language].voir} <span>{projects.name}</span>
             </p>
+          
           </a>
         )}
       </div>
@@ -536,8 +387,8 @@ function Demi({ projects, index }) {
   );
 }
 
-
-function Para() {
+function Para({ language, texts }) {
+  // Ajoutez texts comme prop
   const [scrollPosition, setScrollPosition] = useState(0);
 
   const handleScroll = () => {
@@ -545,9 +396,9 @@ function Para() {
   };
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
@@ -558,7 +409,7 @@ function Para() {
         className="absolute inset-0 z-0"
         style={{
           transform: `translateY(${scrollPosition * 0.2}px)`, // Ajuste pour un mouvement plus fluide
-          transition: 'none', // Aucune transition pour éviter le mouvement après l'arrêt du scroll
+          transition: "none", // Aucune transition pour éviter le mouvement après l'arrêt du scroll
         }}
       >
         <Image
@@ -574,13 +425,15 @@ function Para() {
       <div className="absolute inset-0 bg-white opacity-60 z-10" />
 
       {/* Contenu au-dessus de l'image */}
-      <div className="relative z-20 flex flex-col items-start justify-end h-full text-black pb-32 pl-20">
-    <h1 className="text-3xl text-left">
-        FIND RESIDENTIAL <br /> BUILDING PROJECTS
-    </h1>
-    
-    <p className="text-left text-sm pt-4 flex items-center">SCROLL TO EXPLORE <FaLongArrowAltDown /> </p>
-</div>
+      <div className="relative z-20 flex flex-col items-start justify-end h-full text-black pb-60 pl-20">
+        <h1 className="text-3xl text-left">
+          {texts[language].main} {/* Utiliser le texte basé sur la langue */}
+        </h1>
+
+        <p className="text-left text-sm pt-4 flex items-center">
+          {texts[language].submain} <FaLongArrowAltDown />{" "}
+        </p>
+      </div>
 
       {/* Ajout de contenu supplémentaire pour voir l'effet de parallaxe */}
       <div className="h-screen bg-gray-200 flex items-center justify-center">
