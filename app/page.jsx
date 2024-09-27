fcbc56fe062a107800f488f5c43d8730f18de08e
decadeus <debeaumont@decadeus.com>
2024-09-27 17:22:56 +0200

    loading

--- app/page.jsx
+++ app/page.jsx
@@ -50,6 +50,10 @@ export default function Page() {
     setLanguage(country === "Polska" ? "pl" : "fr"); // Change la langue en fonction du pays
   };
 
+
+  if (loading) {
+    return <Loading />;
+  }
   return (
     <>
       <style jsx>{`
@@ -559,3 +563,16 @@ function ScrollImage({ projects, language, texts }) {
     </div>
   );
 }
+const Loading = () => {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
+    {/* Conteneur du spinner */}
+    <div className="relative flex items-center justify-center h-52 w-52">
+      {/* Texte centré, taille doublée */}
+      <span className="absolute text-4xl font-bold text-brown font-satisfy">Hoomge</span>
+      {/* Cercle tournant */}
+      <div className="animate-spin rounded-full h-full w-full border-t-4 border-b-4 brownborder"></div>
+    </div>
+  </div>
+  );
+};
