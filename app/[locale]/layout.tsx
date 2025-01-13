b619cc7963d32416f4a2c5edee53d024c466d217
decadeus <debeaumont@decadeus.com>
2025-01-13 13:53:03 +0100

    favicon

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -59,8 +59,8 @@ export default async function RootLayout({
       <Head>
         {/* Favicon links */}
         <link rel="icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon.ico" />
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="theme-color" content="#ffffff" />
         
