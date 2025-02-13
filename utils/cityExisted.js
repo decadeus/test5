cce005bd54bbf5943aa5e92057a0353a07bd8600
decadeus <debeaumont@decadeus.com>
2025-02-13 11:01:33 +0100

    bug

--- utils/cityExisted.js
+++ utils/cityExisted.js
@@ -2,23 +2,21 @@ import { createClient } from "@/utils/supabase/client";
 
 const supabase = createClient();
 
-const { data, error } = await supabase
-    .from('project')
-    .select('country, city');
+export const countryDataPromise = (async () => {
+    const { data, error } = await supabase.from('project').select('country, city');
 
-if (error) {
-    console.error('Error fetching cities:', error);
-}
-
-// Transformer en { France: ["Paris", "Lyon"], ... }
-export const countryData = data?.reduce((acc, { country, city }) => {
-    if (!acc[country]) {
-        acc[country] = [];
+    if (error) {
+        console.error('Error fetching cities:', error);
+        return {};
     }
-    if (!acc[country].includes(city)) {
-        acc[country].push(city);
-    }
-    return acc;
-}, {}) || {};
 
-console.log("Exported countryData:", countryData);
+    return data.reduce((acc, { country, city }) => {
+        if (!acc[country]) {
+            acc[country] = [];
+        }
+        if (!acc[country].includes(city)) {
+            acc[country].push(city);
+        }
+        return acc;
+    }, {});
+})();
