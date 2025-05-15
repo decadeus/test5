c353038e4a59de1d8003b41405562d6833595f38
decadeus <debeaumont@decadeus.com>
2025-05-15 13:44:02 +0200

    test

--- app/api/stripe-webhook/route.js
+++ app/api/stripe-webhook/route.js
@@ -30,7 +30,7 @@ export async function POST(req) {
         {
           id: subscription.id,
           customer_id: subscription.customer,
-          email: subscription.custome_email,
+          email: subscription.customer_email,
           status: subscription.status,
           created_at: new Date(subscription.created * 1000).toISOString(),
          
