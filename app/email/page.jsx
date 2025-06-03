8719e5b20b7cfac5f8bb3be88e97d558afe50ae7
decadeus <debeaumont@decadeus.com>
2025-06-03 15:03:52 +0200

    Test

--- app/email/page.jsx
+++ app/email/page.jsx
@@ -1,10 +1,7 @@
 'use client';
-import { useSession } from "next-auth/react";
 import EmailSender from '@/app/[locale]/components/EmailSender';
 
 const Home = () => {
-  const { data: session } = useSession();
-
   return (
     <div>
       <h1>Send an Email</h1>
