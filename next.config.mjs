54ccf14c46e2f8748b447022e5f3a6827c0681ee
decadeus <debeaumont@decadeus.com>
2025-07-02 11:23:28 +0200

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,18 @@
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./app/i18n/request.ts');
-/** @type {import('next').NextConfig} */
+
+const securityHeaders = [
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'Content-Security-Policy-Report-Only',
+    value: "default-src 'self'; img-src * data:; script-src 'self' 'unsafe-inline' https:; style-src 'self' 'unsafe-inline' https:;",
+  },
+];
+
 const nextConfig = {
   images: {
     domains: [
@@ -11,6 +22,14 @@ const nextConfig = {
   },
   experimental: {
     legacyBrowsers: false,
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ];
   },
 };
 
