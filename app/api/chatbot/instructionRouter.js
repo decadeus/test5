f0a79a104c3b7ed845cdf9878d35c04480c7a418
decadeus <debeaumont@decadeus.com>
2025-07-04 10:30:21 +0200

    divers

--- app/api/chatbot/instructionRouter.js
+++ app/api/chatbot/instructionRouter.js
@@ -2,12 +2,14 @@ import seoMetadataInfo from './instructions/seo';
 import abonnementInfo from './instructions/abonnement';
 import origineInfo from './instructions/origine';
 import magiclinkInfo from './instructions/magiclink';
+import promoteurGuide from './instructions/promoteurguide';
 
 const instructionMap = [
   { keywords: ["seo", "référencement", "meta", "langue", "language", "balise", "moteur de recherche"], getInfo: seoMetadataInfo },
   { keywords: ["abonnement", "souscription", "abonner", "abonnements", "subscription", "subscribe"], getInfo: abonnementInfo },
   { keywords: ["créateur", "fondateur", "qui a créé", "qui a cree", "origine", "hoomge.com", "hoomge"], getInfo: origineInfo },
   { keywords: ["magic link", "lien magique", "connexion sans mot de passe", "login sans mot de passe", "magiclink"], getInfo: magiclinkInfo },
+  { keywords: ["lng", "lat", "longitude", "latitude", "coordonnées", "coordonnees"], getInfo: promoteurGuide },
   // Ajoute d'autres blocs ici si besoin
 ];
 
---
+++ app/api/chatbot/instructions/promoteurguide.js
@@ -0,0 +1,3 @@
+export default function prommoteurGuide() {
+  return `\nPour aider le promoteur à remplir la fiche projet :\n- Si vous devez renseigner les coordonnées géographiques (latitude et longitude) du projet, utilisez l'outil "google.com/maps" \n- Cliquez sur le lieu du projet sur la carte : les coordonnées (lat, lng) apparaîtront automatiquement en bas de la page.\n- Vous pouvez alors copier ces valeurs et les renseigner dans la fiche projet.\n`;
+}
