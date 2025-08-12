9ed1ed0be182d3cd502611e7402e69408f0c6155
decadeus <debeaumont@decadeus.com>
2025-08-12 09:30:07 +0200

    Test

--- app/sitemap_fr.xml/route.ts
+++ app/sitemap_fr.xml/route.ts
@@ -48,16 +48,17 @@ export async function GET() {
         `</url>`
       );
     }))
-  ].join('');
+  ].join('\n');
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>\n` +
-    `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">` +
-    urls + `</urlset>`;
+    `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">\n` +
+    urls + `\n</urlset>`;
 
   return new NextResponse(xml, {
     headers: {
-      'Content-Type': 'application/xml; charset=utf-8',
-      'Cache-Control': 'public, s-maxage=3600, stale-while-revalidate=600',
+      'content-type': 'application/xml; charset=utf-8',
+      'x-content-type-options': 'nosniff',
+      'cache-control': 'public, s-maxage=3600, stale-while-revalidate=600',
     },
   });
 } 
